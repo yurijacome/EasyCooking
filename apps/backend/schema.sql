7ed0bbf2c6e859a03ae3cd42d335cda980edff87
Yuri <yurijacome@hotmail.com>
2025-09-23 00:42:29 -0300

    easycookingBase

---
+++ apps/backend/schema.sql
@@ -0,0 +1,12 @@
+
+
+-- tabela usuarios
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    admin BOOLEAN DEFAULT FALSE
+);
