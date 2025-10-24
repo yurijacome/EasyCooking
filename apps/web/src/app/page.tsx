ef83c7eff82c8342ad66eb5954e2ce03071cb152
Yuri <yurijacome@hotmail.com>
2025-10-24 03:09:12 -0300

    implementando google

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,19 +1,27 @@
+"use client";
 import Image from "next/image";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LandingPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    const t = setTimeout(() => {
+      router.push("/Login");
+    }, 5000);
+    return () => clearTimeout(t);
+  }, [router]);
 
   return (
-
     <div className="LandingPage">
-
-        <Image
-          src="/logoBlack.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
+      <Image
+        src="/logoBlack.svg"
+        alt="Next.js logo"
+        width={180}
+        height={38}
+        priority
+      />
     </div>
   );
 }
